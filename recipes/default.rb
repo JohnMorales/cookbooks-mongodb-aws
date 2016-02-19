01cfafee287944819d5a7b5764d873ca2be170c3
JohnMorales <jmorales@gmail.com>
2016-02-19 05:43:07 -0500

    Fixing permissions on conf file

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,14 +4,14 @@ template "/etc/yum.repos.d/mongodb-32.repo" do
   action :create
 end
 
-template "/etc/mongod.conf" do
-  mode 0600
-  source "mongod.conf.erb"
-  action :create
-end
-
 package "mongodb-org" do
   action :install
+end
+
+template "/etc/mongod.conf" do
+  mode 0644
+  source "mongod.conf.erb"
+  action :create
 end
 
 service "mongod" do
